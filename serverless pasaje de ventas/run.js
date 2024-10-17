bb256d174c905248f2eeca56c8abaafa15321bbb
juanmjacobs <juanmjacobs@users.noreply.github.com>
2024-10-17 00:34:09 -0600

    Delete serverless pasaje de ventas/run.js

--- serverless pasaje de ventas/run.js
+++
@@ -1,9 +0,0 @@
-const { extraFunction } = require("./extraFunction.js");
-
-async function handler(context) {
-    const orderNotes = context.resource.notes || "no notes";
-    await createInvoiceIntegration({ integrationId: orderNotes + " - " + new Date().toISOString() });
-    return { result: await extraFunction(orderNotes), context, env: process.env };
-}
-
-module.exports = { handler };
