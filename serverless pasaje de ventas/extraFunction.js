b04b4215e5b94ab2ed52499081df400138cdad34
juanmjacobs <juanmjacobs@users.noreply.github.com>
2024-10-17 02:02:46 -0600

    Update extraFunction.js

--- serverless pasaje de ventas/extraFunction.js
+++ serverless pasaje de ventas/extraFunction.js
@@ -1,6 +1,6 @@
 const { nestedFunction } = require("./nestedfolder/nestedFunction");
 //const badrequire = require("../illegalbadrequire");
 async function extraFunction(param) {
-	return _.toUpper(nestedFunction(param + " extraOverridenFunction!"));
+	return _.toUpper(nestedFunction(param + " extraFunctionOverriden!"));
 }
 module.exports = { extraFunction }
