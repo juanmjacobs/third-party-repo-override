f2bd80a0b4ba16587f1adb3a4d1611f965e62fea
juanmjacobs <juanmjacobs@users.noreply.github.com>
2026-02-26 12:35:35 -0600

    Refactor integrationId assignment in handler function

--- serverless_pasaje_de_ventas/run.js
+++ serverless_pasaje_de_ventas/run.js
@@ -4,7 +4,7 @@ const Promise = require("bluebird");
 async function handler(context) {
     const orderNotes = context.resource.notes || "no notes";
     console.log("Logueo las orderNotes:", orderNotes);
-    const integrationId = await extraFunction("soy un alto override " + new Date().toISOString());
+    const integrationId = "soy un alto override " + new Date().toISOString();
     await ProductecaSdk.salesOrdersApi.update(context.resource.id, { notes: integrationId })
     //await ProductecaSdk.Invoicer.invoice({ createInvoice, validate })
    // await ProductecaSdk.createInvoiceIntegration({ integrationId });
