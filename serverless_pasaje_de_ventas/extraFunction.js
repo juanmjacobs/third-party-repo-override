2e85ece4cf1e0b020316003337b38a998e1e4a7a
juanmjacobs <juanmjacobs@users.noreply.github.com>
2026-02-27 11:52:29 -0600

    Delete serverless_pasaje_de_ventas/extraFunction.js

--- serverless_pasaje_de_ventas/extraFunction.js
+++
@@ -1,6 +0,0 @@
-const { nestedFunction } = require("./nestedfolder/nestedFunction");
-//const badrequire = require("../illegalbadrequire");
-async function extraFunction(param) {
-	return _.toUpper(nestedFunction(param + " extraFunctionOverriden!"));
-}
-module.exports = { extraFunction }
