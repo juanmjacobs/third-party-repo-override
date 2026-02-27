642caeb58e2c9cd09b021eeaf676f1640e6bf7aa
juanmjacobs <juanmjacobs@users.noreply.github.com>
2026-02-27 12:21:01 -0600

    Update extraFunction.js

--- serverless_pasaje_de_ventas/extraFunction.js
+++ serverless_pasaje_de_ventas/extraFunction.js
@@ -1,6 +1,6 @@
 const { nestedFunction } = require("./nestedfolder/nestedFunction");
 //const badrequire = require("../illegalbadrequire");
 async function extraFunction(param) {
-	return _.toUpper(nestedFunction(param + " UNABRANCHLOCA - extraFunctionOverriden!"));
+	return _.toUpper(nestedFunction(param + " UNABRANCHLOCA EN REPO OVERRIDE"));
 }
 module.exports = { extraFunction }
